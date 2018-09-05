6c610cf2161665a1f51c6285f4eb0eccd47b61a3
Alexander Zautke <mail@alexander-zautke.com>
2018-09-05 22:25:11 +0200

    Add dummy DocumentOperationConfiguration

---
+++ VonkDocumentOperation/VonkDocumentOperation/DocumentOperationConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Vonk.Core.Pluggability;
+namespace VonkDocumentOperation
+{
+    [VonkConfiguration(order: 4900)]
+    public static class DocumentOperationConfiguration
+    {
+        public static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            //add services here to the DI system of ASP.NET Core
+            return services;
+        }
+
+        public static IApplicationBuilder Configure(IApplicationBuilder builder)
+        {
+            //add middleware to the pipeline being built with the builder
+            return builder;
+        }
+    }
+}
---
+++ VonkDocumentOperation/VonkDocumentOperation/Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace VonkDocumentOperation
+{
+    public class Startup
+    {
+       static void Main(string[] args)
+        {
+            // Display the number of command line arguments:
+            System.Console.WriteLine("Hello Vonk!");
+        }
+    }
+}
