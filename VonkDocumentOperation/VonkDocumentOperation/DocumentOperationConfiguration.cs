cdbbc75a3f0a88181903aabfecae6d602415a990
Alexander Zautke <mail@alexander-zautke.com>
2018-09-06 11:28:12 +0200

    Test: Log all arguments of a search interaction

--- VonkDocumentOperation/VonkDocumentOperation/DocumentOperationConfiguration.cs
+++ VonkDocumentOperation/VonkDocumentOperation/DocumentOperationConfiguration.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Vonk.Core.Context.Features;
 using Vonk.Core.Pluggability;
+using Vonk.Core.Repository;
+
 namespace VonkDocumentOperation
 {
     [VonkConfiguration(order: 4900)]
     public static class DocumentOperationConfiguration
     {
+        // Add services here to the DI system of ASP.NET Core
         public static IServiceCollection ConfigureServices(IServiceCollection services)
         {
-            //add services here to the DI system of ASP.NET Core
+            services.AddScoped<ISearchRepository, DocumentRepository>(); // Implements the interface for a FHIR READ operation
             return services;
         }
 
---
+++ VonkDocumentOperation/VonkDocumentOperation/DocumentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Vonk.Core.Context;
+using Vonk.Core.Context.Features;
+using Vonk.Core.Repository;
+
+namespace VonkDocumentOperation
+{
+    public class DocumentRepository : ISearchRepository
+    {
+        public DocumentRepository()
+        {
+        }
+
+        public Task<SearchResult> Search(IArgumentCollection arguments, SearchOptions options)
+        {
+            foreach(var argument in arguments){
+                Console.WriteLine(argument);
+            }
+            return null;
+        }
+    }
+}
