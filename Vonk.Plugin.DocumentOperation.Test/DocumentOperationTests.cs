522aa5766dc3a29b3f81b5374169bb99278a0702
Alexander Zautke <alexander@fire.ly>
2019-01-30 11:46:51 +0100

    Add unit tests for DocumentOperationService

---
+++ Vonk.Plugin.DocumentOperation.Test/DocumentOperationTests.cs
@@ -0,0 +1,367 @@
+using System;
+using Hl7.Fhir.Model;
+using Moq;
+using Vonk.Core.Common;
+using Vonk.Core.Context;
+using Vonk.Core.Repository;
+using Vonk.Test.Utils;
+using Xunit;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using static Vonk.Plugin.DocumentOperation.Test.LoggerUtils;
+using Task = System.Threading.Tasks.Task;
+using List = System.Collections.Generic.List<Vonk.Core.Common.IResource>;
+using Vonk.Fhir.R3;
+using Vonk.Core.Context.Features;
+using Vonk.Core.Support;
+using static Hl7.Fhir.Model.Bundle;
+
+namespace Vonk.Plugin.DocumentOperation.Test
+{
+    /*   $document unit tests:
+            - (X) $document should return HTTP 200 and a valid FHIR document (GET & POST)
+            - (X) $document should return HTTP 404 when being called on a missing composition         
+            - (X) $document should persist the generated document on request
+            - (X) $document should return an INVALID_REQUEST when being called with POST and an missing id
+            - (X) $document should thrown an internal server error if a local reference to a resource, which should be included in the document, can't be found.          
+    */
+
+    public class DocumentOperationTests
+    {
+        private DocumentService _documentService;
+
+        private ILogger<DocumentService> _logger = Logger<DocumentService>();
+        private Mock<ISearchRepository> _searchMock = new Mock<ISearchRepository>();
+        private Mock<IResourceChangeRepository> _changeMock = new Mock<IResourceChangeRepository>();
+
+        public DocumentOperationTests()
+        {
+            _documentService = new DocumentService(_searchMock.Object, _changeMock.Object, _logger);
+        }
+
+        [Fact]
+        public async Task DocumentOperationGETReturn200OnSuccess()
+        {
+            // Setup Composition resource
+            var composition = CreateTestCompositionNoReferences();
+            var searchResult = new SearchResult(new List() { composition }, 1, 1);
+            _searchMock.Setup(repo => repo.Search(It.IsAny<IArgumentCollection>(), It.IsAny<SearchOptions>())).ReturnsAsync(searchResult);
+
+            // Create VonkContext for $document (GET / Instance level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition"),
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceId, "test")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "GET";
+
+            // Execute $document
+            await _documentService.DocumentInstanceGET(testContext);
+
+            // Check response status
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status200OK, "$document should succeed with HTTP 200 - OK on test composition");
+            testContext.Response.Payload.Should().NotBeNull();
+            var document = testContext.Response.Payload.ToPoco<Bundle>();
+            document.Type.Should().Be(BundleType.Document);
+        }
+
+        [Fact]
+        public async Task DocumentOperationPOSTReturn200OnSuccess()
+        {
+            // Setup Composition resource
+            var composition = CreateTestCompositionNoReferences();
+            var searchResult = new SearchResult(new List() { composition }, 1, 1);
+            _searchMock.Setup(repo => repo.Search(It.IsAny<IArgumentCollection>(), It.IsAny<SearchOptions>())).ReturnsAsync(searchResult);
+
+            // Create VonkContext for $document (POST / Type level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "POST";
+
+            var parameters = new Parameters();
+            var idValue = new FhirString("example");
+            var parameterComponent = new Parameters.ParameterComponent { Name = "id" };
+            parameterComponent.Value = idValue;
+            parameters.Parameter.Add(parameterComponent);
+
+            testContext.TestRequest.Payload = new RequestPayload(true, parameters.ToIResource());
+
+            // Execute $document
+            await _documentService.DocumentTypePOST(testContext);
+
+            // Check response status
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status200OK, "$document should succeed with HTTP 200 - OK on test composition");
+            testContext.Response.Payload.Should().NotBeNull();
+            var document = testContext.Response.Payload.ToPoco<Bundle>();
+            document.Type.Should().Be(BundleType.Document);
+        }
+
+        [Fact]
+        public async Task DocumentOperationPOSTReturn400OnMissingId()
+        {
+            // Setup Composition resource
+            var composition = CreateTestCompositionNoReferences();
+            var searchResult = new SearchResult(new List() { composition }, 1, 1);
+            _searchMock.Setup(repo => repo.Search(It.IsAny<IArgumentCollection>(), It.IsAny<SearchOptions>())).ReturnsAsync(searchResult);
+
+            // Create VonkContext for $document (POST / Type level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "POST";
+            testContext.TestRequest.Payload = new RequestPayload(true, new Parameters().ToIResource()); // Call with empty parameters
+
+            // Execute $document
+            await _documentService.DocumentTypePOST(testContext);
+
+            // Check response status
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status400BadRequest, "$document should fail with HTTP 400 - Bad request if Parameters resource does not contain an id");
+            testContext.Response.Outcome.Should().NotBeNull();
+            testContext.Response.Outcome.Issue.Should().Contain(issue => issue.Code.Equals(VonkIssues.INVALID_REQUEST.Type));
+        }
+
+        [Fact]
+        public async Task DocumentOperationGETReturn404MissingComposition()
+        {
+            // Let ISearchRepository return no Composition
+            var composition = CreateTestCompositionNoReferences();
+            var searchResult = new SearchResult(new List() , 0, 0);
+            _searchMock.Setup(repo => repo.Search(It.IsAny<IArgumentCollection>(), It.IsAny<SearchOptions>())).ReturnsAsync(searchResult);
+
+            // Create VonkContext for $document (GET / Instance level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition"),
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceId, "test")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "GET";
+
+            // Execute $document without creating a Composition first
+            await _documentService.DocumentInstanceGET(testContext);
+
+            // Check response status
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status404NotFound, "$document should return HTTP 404 - Not found when called on a missing composition");
+        }
+
+        [Fact]
+        public async Task DocumentOperationShouldPersistBundle()
+        {
+            // Setup Composition resource
+            var composition = CreateTestCompositionNoReferences();
+            var searchResult = new SearchResult(new List() { composition }, 1, 1);
+            _searchMock.Setup(repo => repo.Search(It.IsAny<IArgumentCollection>(), It.IsAny<SearchOptions>())).ReturnsAsync(searchResult);
+
+            // Create VonkContext for $document (GET / Instance level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition"),
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceId, "test"),
+                new Argument(ArgumentSource.Query, "persist", "true")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "GET";
+
+            // Execute $document
+            await _documentService.DocumentInstanceGET(testContext);
+
+            _changeMock.Verify(c => c.Create(It.IsAny<IResource>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task DocumentOperationInternalServerErrorOnMissingReference1()
+        {
+            // Setup Composition resource
+            var composition = CreateTestCompositionInclPatient(); // Unresolvable reference (patient resource) in the composition resource (1. level)
+            var compositionSearchResult = new SearchResult(new List() { composition }, 1, 1);
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("Composition")), It.IsAny<SearchOptions>())).ReturnsAsync(compositionSearchResult);
+
+            // Create VonkContext for $document (GET / Instance level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition"),
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceId, "test")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "GET";
+
+            // Execute $document
+            await _documentService.DocumentInstanceGET(testContext);
+
+            // Check response status
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status500InternalServerError, "$document should return HTTP 500 - Internal Server error when a reference which is referenced by the composition");
+        }
+
+        [Fact]
+        public async Task DocumentOperationInternalServerErrorOnMissingReference2()
+        {
+            // Setup Composition resource
+            var composition = CreateTestCompositionInclPatient(); // Unresolvable reference (Practitioner resource) in patient resource (2. level)
+            var compositionSearchResult = new SearchResult(new List() { composition }, 1, 1);
+
+            var patient = CreateTestPatient();
+            var patientSearchResult = new SearchResult(new List() { patient }, 1, 1);
+
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("Composition")), It.IsAny<SearchOptions>())).ReturnsAsync(compositionSearchResult);
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("Patient")), It.IsAny<SearchOptions>())).ReturnsAsync(patientSearchResult);
+
+            // Create VonkContext for $document (GET / Instance level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition"),
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceId, "test")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "GET";
+
+            // Execute $document
+            await _documentService.DocumentInstanceGET(testContext);
+
+            // Check response status
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status500InternalServerError, "$document should return HTTP 500 - Internal Server error when a reference which is referenced by the composition");
+        }
+
+        [Fact]
+        public async Task DocumentOperationInternalServerErrorOnMissingReference3()
+        {
+            // Setup Composition resource
+            var composition = CreateTestCompositionInclList(); // Unresolvable reference (Medication resource) in MedicationStatement resource (4. level)
+            var compositionSearchResult = new SearchResult(new List() { composition }, 1, 1);
+
+            var list = CreateTestList();
+            var listSearchResults = new SearchResult(new List { list }, 1, 1);
+
+            var medcationStatement = CreateTestMedicationStatement();
+            var medcationStatementSearchResult = new SearchResult(new List() { medcationStatement }, 1, 1);
+
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("Composition")), It.IsAny<SearchOptions>())).ReturnsAsync(compositionSearchResult);
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("List")), It.IsAny<SearchOptions>())).ReturnsAsync(listSearchResults);
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("MedicationStatement")), It.IsAny<SearchOptions>())).ReturnsAsync(medcationStatementSearchResult);
+
+            // Create VonkContext for $document (GET / Instance level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition"),
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceId, "test")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "GET";
+
+            // Execute $document
+            await _documentService.DocumentInstanceGET(testContext);
+
+            // Check response status
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status500InternalServerError, "$document should return HTTP 500 - Internal Server error when a reference which is referenced by the composition");
+        }
+
+        [Fact]
+        public async Task DocumentOperationSuccessCompleteComposition()
+        {
+            // Setup Composition resource
+            var composition = CreateTestCompositionInclList(); // Unresolvable reference (Medication resource) in MedicationStatement resource (4. level)
+            var compositionSearchResult = new SearchResult(new List() { composition }, 1, 1);
+
+            var list = CreateTestList();
+            var listSearchResults = new SearchResult(new List { list }, 1, 1);
+
+            var medcationStatement = CreateTestMedicationStatement();
+            var medcationStatementSearchResult = new SearchResult(new List() { medcationStatement }, 1, 1);
+
+            var medication = CreateTestMedication();
+            var medicationSearchResult = new SearchResult(new List { medication }, 1, 1);
+
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("Composition")), It.IsAny<SearchOptions>())).ReturnsAsync(compositionSearchResult);
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("List")), It.IsAny<SearchOptions>())).ReturnsAsync(listSearchResults);
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("MedicationStatement")), It.IsAny<SearchOptions>())).ReturnsAsync(medcationStatementSearchResult);
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("Medication")), It.IsAny<SearchOptions>())).ReturnsAsync(medcationStatementSearchResult);
+
+            // Create VonkContext for $document (GET / Instance level)
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition"),
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceId, "test")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "GET";
+
+            // Execute $document
+            await _documentService.DocumentInstanceGET(testContext);
+
+            // Check response status
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status200OK, "$document should return HTTP 200 - OK when all references in the composition (incl. recursive references) can be resolved");
+        }
+
+        // $document is expected to fail if a resource reference is missing, this should be checked on all levels of recursion.
+        // Therefore, we build multiple resources, each with different unresolvable references
+
+        private IResource CreateTestCompositionNoReferences()
+        {
+            return new Composition() { Id = "test", VersionId = "v1" }.ToIResource();
+        }
+
+        private IResource CreateTestCompositionInclPatient()
+        {
+            return new Composition() { Id = "test", VersionId = "v1", Subject = new ResourceReference("Patient/test") }.ToIResource();
+        }
+
+        private IResource CreateTestCompositionInclList()
+        {
+            var composition = new Composition() { Id = "test", VersionId = "v1" };
+            var sectionComponent = new Composition.SectionComponent();
+            sectionComponent.Entry.Add(new ResourceReference("List/test"));
+            composition.Section.Add(sectionComponent);
+
+            return composition.ToIResource();
+        }
+
+        private IResource CreateTestPatient()
+        {
+            var patient = new Patient { Id = "test" };
+            patient.GeneralPractitioner.Add(new ResourceReference("Practitioner/missing"));
+
+            return patient.ToIResource();
+        }
+
+        private IResource CreateTestList()
+        {
+            var list = new Hl7.Fhir.Model.List { Id = "test" };
+            var entryComponent = new Hl7.Fhir.Model.List.EntryComponent();
+            entryComponent.Item = new ResourceReference("MedicationStatement/test");
+            list.Entry.Add(entryComponent);
+
+            return list.ToIResource();
+        }
+
+        private IResource CreateTestMedicationStatement()
+        {
+            var medication = new ResourceReference("Medication/test");
+            return new MedicationStatement { Id = "test", Medication = medication }.ToIResource();
+        }
+
+        private IResource CreateTestMedication()
+        {
+            return new Medication { Id = "test" }.ToIResource();
+        }
+
+        private IResource CreateBundle()
+        {
+            return new Bundle() { Id = "test", VersionId = "v1" }.ToIResource();
+        }
+    }
+}
