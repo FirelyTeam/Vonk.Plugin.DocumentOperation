588a2777b5f22c1611888e003d70ab6d84c9b0c8
Alexander Zautke <alexander@fire.ly>
2019-05-19 16:55:30 +0200

    Check if custom resources can be included in a document bundle

---
+++ Vonk.Plugin.DocumentOperation.Test/CustomResourcesInDocumentTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using Hl7.Fhir.Specification;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Vonk.Core.Common;
+using Vonk.Core.Context;
+using Vonk.Core.ElementModel;
+using Vonk.Core.Repository;
+using Vonk.Fhir.R3;
+using Vonk.Test.Utils;
+using Xunit;
+using static Vonk.Plugin.DocumentOperation.Test.LoggerUtils;
+using Task = System.Threading.Tasks.Task;
+
+namespace Vonk.Plugin.DocumentOperation.Test
+{
+    public class CustomResourcesInDocumentTests
+    {
+        private readonly DocumentService _documentService;
+        private readonly ILogger<DocumentService> _logger = Logger<DocumentService>();
+        private readonly Mock<ISearchRepository> _searchMock = new Mock<ISearchRepository>();
+        private readonly Mock<IResourceChangeRepository> _changeMock = new Mock<IResourceChangeRepository>();
+        private readonly IStructureDefinitionSummaryProvider _schemaProvider;
+
+        public CustomResourcesInDocumentTests()
+        {
+            var reader = new TestResourceReader();
+            var customBasicStructureDefinitionJson = reader.GetResourceAsString("CustomBasic-StructureDefinition-R3.json");
+            var customBasicStructureDefinition = new FhirJsonParser().Parse<StructureDefinition>(customBasicStructureDefinitionJson);
+
+            _schemaProvider = SchemaProviders.CreateCustomSchemaProvider(customBasicStructureDefinition);
+            _documentService = new DocumentService(_searchMock.Object, _changeMock.Object, _schemaProvider, _logger);
+        }
+
+        [Fact]
+        public async Task DocumentOperationCanIncludeCustomResources()
+        {
+            var composition = CreateTestCompositionInclCustomResource();
+            var compositionSearchResult = new SearchResult(new List<IResource>() { composition }, 1, 1);
+
+            var customResourceTest = SourceNode.Resource("CustomBasic", "CustomBasic");
+            customResourceTest.Add(SourceNode.Valued("id", Guid.NewGuid().ToString()));
+            var customResourceSearchResults = new SearchResult(new List<IResource> { customResourceTest.ToIResource() }, 1, 1);
+
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("Composition")), It.IsAny<SearchOptions>())).ReturnsAsync(compositionSearchResult);
+            _searchMock.Setup(repo => repo.Search(It.Is<IArgumentCollection>(arg => arg.GetArgument("_type").ArgumentValue.Equals("CustomBasic")), It.IsAny<SearchOptions>())).ReturnsAsync(customResourceSearchResults);
+
+            var testContext = new VonkTestContext(VonkInteraction.instance_custom);
+            testContext.Arguments.AddArguments(new[]
+            {
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceType, "Composition"),
+                new Argument(ArgumentSource.Path, ArgumentNames.resourceId, "test")
+            });
+            testContext.TestRequest.CustomOperation = "document";
+            testContext.TestRequest.Method = "GET";
+
+            await _documentService.DocumentInstanceGET(testContext);
+
+            testContext.Response.HttpResult.Should().Be(StatusCodes.Status200OK, "$document should return HTTP 200 - OK when all references in the composition (incl. recursive references) can be resolved");
+            testContext.Response.Payload.SelectNodes("entry.resource").Count().Should().Be(2, "Expected Composition and CustomBasic to be in the document");
+        }
+
+        private IResource CreateTestCompositionInclCustomResource()
+        {
+            return new Composition() { Id = "test", VersionId = "v1", Subject = new ResourceReference("CustomBasic/test") }.ToIResource();
+        }
+    }
+}
---
+++ Vonk.Plugin.DocumentOperation.Test/TestResourceReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Vonk.Test.Utils
+{
+    public class TestResourceReader
+    {
+        private Assembly _resourcesAssembly;
+        private Func<string, string> _getResourceName;
+
+        public TestResourceReader(Type assemblyContainingType = null)
+        {
+            if (assemblyContainingType == null)
+                assemblyContainingType = this.GetType();
+
+            _resourcesAssembly = assemblyContainingType.GetTypeInfo().Assembly;
+            var assemblyName = _resourcesAssembly.GetName().Name;
+
+            _getResourceName = (filename) =>
+            {
+                //Make it work for minimally and maximally specified filenames.
+                if (!Path.HasExtension(filename))
+                    filename = $"{filename}.json";
+                if (!filename.StartsWith($"{assemblyName}.Resources."))
+                    filename = $"{assemblyName}.Resources.{filename}";
+                return filename;
+            };
+        }
+
+        public string GetResourceAsString(string filename)
+        {
+            using (var sr = new StreamReader(GetResourceAsStream(filename)))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+        public IEnumerable<string> GetAllResourcesAsString(string extension = null)
+        {
+            foreach (var resourceName in _resourcesAssembly.GetManifestResourceNames())
+            {
+                if (String.IsNullOrEmpty(extension) || resourceName.EndsWith(extension))
+                    yield return GetResourceAsString(resourceName);
+            }
+        }
+
+        /// <summary>
+        /// Be sure to dispose of the stream yourself!
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public Stream GetResourceAsStream(string filename)
+        {
+            filename = _getResourceName(filename);
+            if (!_resourcesAssembly.GetManifestResourceNames().Contains(filename))
+                throw new ArgumentException($"No such file in the resources: {filename}");
+            return _resourcesAssembly.GetManifestResourceStream(_getResourceName(filename));
+        }
+    }
+}
